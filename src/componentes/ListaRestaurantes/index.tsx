fa63fd6f17bb04457ee424acc75c0bec39590293
jun1rox <junior_alaniz@hotmail.com>
2022-06-17 00:00:53 -0300

    nova paginação para a lista de restaurantes

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -9,28 +9,23 @@ const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('');
+  const [paginaAnterior, setPaginaAnterior] = useState('');
 
-  useEffect(() => {
-    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+  const carregarDados = (url: string) => {
+    axios.get<IPaginacao<IRestaurante>>(url)
       .then(resposta => {
         setRestaurantes(resposta.data.results);
         setProximaPagina(resposta.data.next);
+        setPaginaAnterior(resposta.data.previous);
       })
       .catch(erro => {
         console.log(erro);
-      });
-  }, []);
-
-  const verMais = () => {
-    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
-      .then(resposta => {
-        setRestaurantes([...restaurantes, ...resposta.data.results]);
-        setProximaPagina(resposta.data.next);
       })
-      .catch(erro => {
-        console.log(erro);
-      });
   }
+
+  useEffect(() => {
+    carregarDados('http://localhost:8000/api/v1/restaurantes/')
+  }, []);
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -38,10 +33,12 @@ const ListaRestaurantes = () => {
       {restaurantes?.map(item =>
         <Restaurante restaurante={item} key={item.id} />
       )}
-      {proximaPagina &&
-        <button onClick={verMais}>
-          Ver mais
-        </button>}
+      {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
+        Página Anterior
+      </button>}
+      {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}>
+        Próxima Página
+      </button>}
     </section>
   );
 }
